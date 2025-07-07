78bf8bb134f53ee5cca7a63569d920a83c3f0810
ribdot <rick.fr.simon@gmail.com>
2025-07-07 14:00:12 -0700

    typescript installed

--- resources/js/Pages/Index.svelte
+++ resources/js/Pages/Index.svelte
@@ -1,48 +1,9 @@
-<script>
-    import { onMount } from 'svelte';
-    import { inputManager } from "@/Classes/InputManager.svelte.js";
-    import WordBoardComponent from "@/GameComponents/WordBoardComponent.svelte";
-    import WordBoard from "@/Classes/WordBoard.svelte.js";
-    import KeyboardComponent from "@/GameComponents/KeyboardComponent.svelte";
+<script lang="ts">
+    let name: string = 'World';
 
-    function handleKeyDown(event) {
-        inputManager.handleKeyDown(event.key, wordBoards)
-    }
-
-    function handleKeyUp(event) {
-        inputManager.handleKeyUp(event.key)
-    }
-
-    function handleBlur() {
-        inputManager.handleBlur()
-    }
-
-    onMount(() => {
-        window.addEventListener('keydown', handleKeyDown)
-        window.addEventListener('keyup', handleKeyUp)
-        window.addEventListener('blur', handleBlur)
-
-        return () => {
-            window.removeEventListener('keydown', handleKeyDown)
-            window.removeEventListener('keyup', handleKeyUp)
-            window.removeEventListener('blur', handleBlur)
-        }
-    })
-
-    let wordBoards = [
-        new WordBoard("scoop", 8),
-        new WordBoard("might", 8),
-        new WordBoard("olive", 8),
-        new WordBoard("piano", 8),
-    ]
+    let count: number = $state(0);
 </script>
 
-<div>
-    <div class="board-container grid grid-cols-2 gap-4">
-        {#each wordBoards as wordBoard}
-            <WordBoardComponent wordBoard={wordBoard} />
-        {/each}
-    </div>
+<h1>Hello {name}!</h1>
 
-    <KeyboardComponent wordBoards={wordBoards} />
-</div>
+<button onclick={() => count++}>{count}</button>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import {svelte, vitePreprocess} from "@sveltejs/vite-plugin-svelte";
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    preprocess: [vitePreprocess()],
     plugins: [
         svelte({}),
         laravel({
